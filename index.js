6477ee9d4f0c22be628d776552bda42e9462f06e
Felipe Oliveira <87993401+JFOM-FelipeOliveira@users.noreply.github.com>
2023-04-10 10:41:09 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 var express = require('express');
 var app = express();
+var cors = require('cors');
 
-app.get('/hello', function(req, res){
-    res.send("Hello World!");
+app.use(cors());
+
+app.get('/:id', function enviar(req, res){
+    res.send("The id you specified is: " + req.params.id);
+});
+
+app.get('/soma/:op1/:op2', function enviar(req, res){
+    let op1 = parseInt(req.params.op1)
+    let op2 = parseInt(req.params.op2)
+    let result = op1 + op2
+    res.send(result.toString())
 });
 
 app.post('/hello', function(req, res){
     res.send("You just called the post method at '/hello'!\n");
 });
 
+app.all('/hello', function(req, res){
+    res.send("HTTP method All!");
+});
 
 app.listen(3000, function(req, res){
-    console.log('Servidor ouvindo na posta 3000!');
+    console.log("Servidor ouvindo na posta 3000!");
 });
